b365f2d4db5a39a5b8e47dd19635823ce5462556
Karol Kozak <karol.kozak@fortedigital.no>
2018-12-12 12:07:11 +0100

    Fix using UrlHelper instead of UrlResolver

--- UrlRedirects/UrlRewritePlugin/UrlRewriteModule.cs
+++ UrlRedirects/UrlRewritePlugin/UrlRewriteModule.cs
@@ -31,7 +31,7 @@ namespace UrlRedirects.UrlRewritePlugin
 
         private static void EventsPublishedContent(object sender, ContentEventArgs e)
         {
-            var urlHelper = ServiceLocator.Current.GetInstance<UrlHelper>();
+            var urlHelper = ServiceLocator.Current.GetInstance<IUrlResolver>();
             var cvr = ServiceLocator.Current.GetInstance<IContentVersionRepository>();
             var lastVersion = cvr
                 .List(e.ContentLink)
@@ -40,7 +40,7 @@ namespace UrlRedirects.UrlRewritePlugin
                 .FirstOrDefault();
 
             if (lastVersion == null) return;
-            var oldUrl = urlHelper.ContentUrl(lastVersion.ContentLink);
+            var oldUrl = urlHelper.GetUrl(lastVersion.ContentLink);
 
             var contentRepository = ServiceLocator.Current.GetInstance<IContentRepository>();
             var pageData = contentRepository.Get<IContentData>(lastVersion.ContentLink) as PageData;
@@ -90,8 +90,8 @@ namespace UrlRedirects.UrlRewritePlugin
             var cvr = ServiceLocator.Current.GetInstance<IContentVersionRepository>();
             if (cvr.List(e.ContentLink).Any(p => p.Status == VersionStatus.Published || p.Status == VersionStatus.PreviouslyPublished)) return;
 
-            var urlHelper = ServiceLocator.Current.GetInstance<UrlHelper>();
-            var oldUrl = urlHelper.ContentUrl(e.ContentLink);
+            var urlHelper = ServiceLocator.Current.GetInstance<IUrlResolver>();
+            var oldUrl = urlHelper.GetUrl(e.ContentLink);
 
             e.Items.Add(_oldUrlKey, oldUrl);
         }
@@ -101,8 +101,8 @@ namespace UrlRedirects.UrlRewritePlugin
             var oldUrl = e.Items[_oldUrlKey]?.ToString();
             if (oldUrl != null)
             {
-                var urlHelper = ServiceLocator.Current.GetInstance<UrlHelper>();
-                var newUrl = urlHelper.ContentUrl(e.ContentLink);
+                var urlHelper = ServiceLocator.Current.GetInstance<IUrlResolver>();
+                var newUrl = urlHelper.GetUrl(e.ContentLink);
 
                 if(newUrl != oldUrl)
                 {
