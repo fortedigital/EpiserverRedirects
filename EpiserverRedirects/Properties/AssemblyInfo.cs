c98c853fbc4e5b160ccf89fd258a4f535040526d
Maciej Ekiert <maciej.ekiert@fortedigital.no>
2019-05-22 16:34:04 +0200

    Major version incremented to 2

--- EpiserverRedirects/Properties/AssemblyInfo.cs
+++ EpiserverRedirects/Properties/AssemblyInfo.cs
@@ -1,14 +1,15 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RedirectTests")]
+[assembly: AssemblyTitle("EpiserverRedirects")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RedirectTests")]
+[assembly: AssemblyCompany("Forte_ Digital")]
+[assembly: AssemblyProduct("EpiserverRedirects")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -18,9 +19,6 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("8975F6BC-1176-4DC9-9618-ED6CDC3241DE")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -28,8 +26,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0.0")]
 [assembly: AssemblyFileVersion("2.0.0.0")]
