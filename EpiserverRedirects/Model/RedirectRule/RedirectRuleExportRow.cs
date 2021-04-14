ed72f6ceb514ac05ae1a0fe69f10cb67a6db74da
Wojciech Strzeboński <strzebonsky@gmail.com>
2021-04-14 09:07:28 +0200

    Exctracted export RedirectRule to another class and added missing columns from UI table

---
+++ EpiserverRedirects/Model/RedirectRule/RedirectRuleExportRow.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using Castle.Core.Internal;
+using CsvHelper.Configuration.Attributes;
+using Settings = Forte.EpiserverRedirects.Configuration.Configuration;
+
+
+namespace Forte.EpiserverRedirects.Model.RedirectRule
+{
+    public class RedirectRuleExportRow
+    {
+        [Index(0)]
+        public string OldPattern { get; set; }
+        
+        [Index(1)]
+        public string NewPattern { get; set; }
+
+        [Index(2), Optional]
+        public int? ContentId { get; set; }
+        
+        [Index(3)]
+        public string RedirectRuleType { get; set; }
+        
+        [Index(4)]
+        public string RedirectType { get; set; }
+        
+        [Index(5), Optional]
+        public byte? Priority { get; set; }
+        
+        [Index(6)]
+        public string RedirectOrigin { get; set; }
+
+        [Index(7)]
+        public string IsActive { get; set; }
+        
+        [Index(8)]
+        public string CreatedOn { get; set; }
+        
+        [Index(9)]
+        public string CreatedBy { get; set; }
+        
+        [Index(10)]
+        public string Notes { get; set; }
+        
+        public static RedirectRuleExportRow CreateFromRedirectRule(RedirectRule redirectRule)
+        {
+            return new RedirectRuleExportRow
+            {
+                OldPattern = redirectRule.OldPattern,
+                NewPattern = redirectRule.NewPattern,
+                ContentId = redirectRule.ContentId,
+                RedirectType = redirectRule.RedirectType.ToString(),
+                RedirectRuleType = redirectRule.RedirectRuleType.ToString(),
+                Priority = Convert.ToByte(redirectRule.Priority),
+                RedirectOrigin = redirectRule.RedirectOrigin.ToString(),
+                IsActive = redirectRule.IsActive.ToString().ToUpper(),
+                CreatedOn = redirectRule.CreatedOn.ToString("dd/MM/yyyy H:mm:ss"),
+                CreatedBy = redirectRule.CreatedBy,
+                Notes = redirectRule.Notes
+            };
+        }
+    }
+}
