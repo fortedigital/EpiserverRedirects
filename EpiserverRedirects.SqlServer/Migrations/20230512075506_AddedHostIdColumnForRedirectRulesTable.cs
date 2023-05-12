a0bb8ba8625e86a76ce51f590d2b0d9472b5d6a3
AgnieszkaJania <agnieszka.jania@fortedigital.com>
2023-05-12 13:30:23 +0200

    added migration to add column HostId to table RedirectRules, updated tests for mapping to entity

---
+++ EpiserverRedirects.SqlServer/Migrations/20230512075506_AddedHostIdColumnForRedirectRulesTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Forte.EpiserverRedirects.SqlServer.Migrations
+{
+    public partial class AddedHostIdColumnForRedirectRulesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "HostId",
+                table: "RedirectRules",
+                type: "uniqueidentifier",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HostId",
+                table: "RedirectRules");
+        }
+    }
+}
