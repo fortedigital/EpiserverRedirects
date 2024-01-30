054de0f2298a3b3548b63c4714f6a74e8f04f52b
Kamil Krańczuk <kamil.kranczuk@fortedigital.com>
2024-01-30 09:39:02 +0100

    Add ContentProviderStore

---
+++ EpiserverRedirects/Menu/ContentProviders/ContentProviderStore.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EPiServer.Shell.Services.Rest;
+using Forte.EpiserverRedirects.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace Forte.EpiserverRedirects.Menu.ContentProviders;
+
+[RestStore("ContentProviderStore")]
+public class ContentProviderStore : RestControllerBase
+{
+    private readonly ContentProvidersOptions _contentProvidersOptions;
+    private static readonly ContentProviderOptionDto[] DefaultOptions = { new(ContentProviderConstants.AllId, ContentProviderConstants.AllName)};
+
+    public ContentProviderStore(IOptions<ContentProvidersOptions> contentProvidersOptions)
+    {
+        _contentProvidersOptions = contentProvidersOptions.Value;
+    }
+
+    [HttpGet]
+    public ActionResult GetContentProviders()
+    {
+        var contentProviderOptions = GetContentProviderOptions();
+        
+        return Rest(contentProviderOptions);
+    }
+
+
+    [HttpGet]
+    public ActionResult GetContentProvidersForFilter()
+    {
+        var contentProviderOptions = GetContentProviderOptions();
+        
+        return Rest(DefaultOptions.Concat(contentProviderOptions));
+    }
+
+    private IEnumerable<ContentProviderOptionDto> GetContentProviderOptions()
+    {
+        return _contentProvidersOptions
+            .ContentProviders
+            .Select(x => new ContentProviderOptionDto(x.Id, x.Name));
+    }
+
+}
